0b8615325a9ae22f20c1ab16f9a9aabd4ba41257
r1oga <r1oga@tuta.io>
2021-01-16 17:58:11 +0100

    First commit

---
+++ server/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    return render_template("index.html")
+
+
+@app.route("/sucess", methods=["POST"])
+def success():
+    return render_template("success.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
