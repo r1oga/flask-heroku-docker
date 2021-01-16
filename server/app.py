97b3028919e080c19c6157b13276f9285e05d3b4
r1oga <r1oga@tuta.io>
2021-01-16 19:21:11 +0100

    feat: Send email to user upon submit

--- server/app.py
+++ server/app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from send_email import send_email
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -28,11 +32,15 @@ def success():
     if request.method == "POST":
         email, height = request.form.values()
 
-        data = Data(email, height)
-        db.session.add(data)
-        db.session.commit()
+        # check if user already exists
+        if db.session.query(Data).filter(Data.email == email).count() == 0:
+            send_email(email, height)
+            data = Data(email, height)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("success.html")
 
-        return render_template("success.html")
+    return render_template("index.html", text="This email already submitted a height!")
 
 
 @app.route("/average", methods=["GET"])
---
+++ server/send_email.py
@@ -0,0 +1,22 @@
+from email.mime.text import MIMEText
+import os
+import smtplib
+
+
+def send_email(email, height):
+    from_email = os.getenv("EMAIL")
+    from_password = os.getenv("PASSWORD")
+    to_email = email
+
+    subject = "Height data"
+    msg = f"Hey there, your height is <strong>{height}</strong>."
+    body = MIMEText(msg, "html")
+    body["Subject"] = subject
+    body["To"] = to_email
+    body["From"] = from_email
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(from_email, from_password)
+    gmail.send_message(body)
